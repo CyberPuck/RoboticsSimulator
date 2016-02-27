227f55424bc85bb3e72dd5a773cbcb148c6f461c
Kyle <traverskyle9@gmail.com>
2016-02-27 16:02:10 -0500

    Updating UI and adding in the base tab handlers for all required modes.

--- src/main/src/UI/tabControls/PointTab.java
+++ src/main/src/UI/tabControls/PointTab.java
@@ -1,9 +1,28 @@
 package UI.tabControls;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 /**
  * Handles the point tab UI.
  *
  * Created by CyberPuck on 2016-02-24.
  */
 public class PointTab {
+    @FXML
+    private TextField xEndPoint;
+    @FXML
+    private TextField yEndPoint;
+    @FXML
+    private TextField endOrientation;
+    @FXML
+    private TextField speed;
+    @FXML
+    private TextField timeToFinish;
+    @FXML
+    private Button startButton;
+
+    public PointTab() {}
 }
--- src/main/src/UI/tabControls/WayPointTab.java
+++ src/main/src/UI/tabControls/WayPointTab.java
@@ -1,9 +1,30 @@
 package UI.tabControls;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
  * Handles the way point tab UI.
  *
  * Created by CyberPuck on 2016-02-24.
  */
 public class WayPointTab {
+    @FXML
+    private TextField xEndPoint;
+    @FXML
+    private TextField yEndPoint;
+    @FXML
+    private TextArea wayPoint;
+    @FXML
+    private TextField rotation;
+    @FXML
+    private TextField speed;
+    @FXML
+    private TextField time;
+    @FXML
+    private Button startButton;
+
+    public WayPointTab() {}
 }
