fe47823e1f6cd8e316da988970db9613373df3e1
Kyle <traverskyle9@gmail.com>
2016-03-04 01:02:40 -0500

    Adding point UI logic, simulator will now be feed input for a point path.

--- src/main/src/UI/tabControls/PointTab.java
+++ src/main/src/UI/tabControls/PointTab.java
@@ -1,15 +1,25 @@
 package UI.tabControls;
 
+import UI.SimulatorController;
+import inputs.PointInput;
+import inputs.RobotInput;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import utilities.Point;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Handles the point tab UI.
  * <p/>
  * Created by CyberPuck on 2016-02-24.
  */
-public class PointTab {
+public class PointTab implements Initializable {
     @FXML
     private TextField xEndPoint;
     @FXML
@@ -23,6 +33,55 @@ public class PointTab {
     @FXML
     private Button startButton;
 
-    public PointTab() {
+    private SimulatorController controller;
+
+    public PointTab(SimulatorController controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        startButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (controller.isSimulatorRunning()) {
+                    // send the stop command
+                    startButton.setText("Start");
+                    controller.stopSimulator();
+                } else {
+                    // start the simulation
+                    RobotInput input = formatInput();
+                    startButton.setText("Stop");
+                    controller.startSimulator(input);
+                }
+            }
+        });
+        // set defaults
+        setDefaults();
+    }
+
+    /**
+     * Sets defaults for the input fields.
+     */
+    private void setDefaults() {
+        xEndPoint.setText("0.0");
+        yEndPoint.setText("0.0");
+        speed.setText("0.0");
+        endOrientation.setText("0.0");
+        timeToFinish.setText("0.0");
+    }
+
+    /**
+     * Formats the UI inputs.
+     *
+     * @return PointInput for the simulator
+     */
+    private RobotInput formatInput() {
+        double x = Double.parseDouble(this.xEndPoint.getText());
+        double y = Double.parseDouble(this.yEndPoint.getText());
+        double endOrientation = Double.parseDouble(this.endOrientation.getText());
+        double speed = Double.parseDouble(this.speed.getText());
+        double time = Double.parseDouble(this.timeToFinish.getText());
+        return new PointInput(new Point(x, y), speed, endOrientation, time);
     }
 }
--- src/main/src/inputs/PointInput.java
+++ src/main/src/inputs/PointInput.java
@@ -1,13 +1,56 @@
 package inputs;
 
+import utilities.Point;
+
 /**
+ * Handles the end point control.
  * Created by CyberPuck on 2016-03-01.
  */
 public class PointInput implements RobotInput {
     private InputMode mode = InputMode.POINT;
 
+    // end point in feet
+    private Point endPoint;
+    // speed in feet/sec
+    private double speed;
+    private double endOrientation;
+    private double time;
+
+    public PointInput(Point endPoint, double speed, double endOrientation, double time) {
+        this.endPoint = endPoint;
+        this.endOrientation = endOrientation;
+        this.time = time;
+    }
+
     @Override
     public InputMode getMode() {
         return mode;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getEndOrientation() {
+        return endOrientation;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "PointInput{" +
+                "mode=" + mode +
+                ", endPoint=" + endPoint +
+                ", speed=" + speed +
+                ", endOrientation=" + endOrientation +
+                ", time=" + time +
+                '}';
     }
 }
